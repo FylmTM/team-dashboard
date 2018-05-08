086d0b30357e1f4a11199d2e9fa0854b7acb7905
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2018-05-08 20:42:05 +0100

    Initial commit

---
+++ backend/src/main/kotlin/me/vrublevsky/team/dashboard/DashboardApplication.kt
@@ -0,0 +1,11 @@
+package me.vrublevsky.team.dashboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DashboardApplication
+
+fun main(args: Array<String>) {
+    runApplication<DashboardApplication>(*args)
+}
