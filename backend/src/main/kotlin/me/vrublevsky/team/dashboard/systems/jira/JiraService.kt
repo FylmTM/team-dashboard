550297df0cb0d21283559b1549e2a27572ca0e8d
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2018-05-21 19:12:52 +0100

    Nullify board if no active sprint

--- backend/src/main/kotlin/me/vrublevsky/team/dashboard/systems/jira/JiraService.kt
+++ backend/src/main/kotlin/me/vrublevsky/team/dashboard/systems/jira/JiraService.kt
@@ -29,61 +29,72 @@ class JiraService(
         dashboardConfiguration.teams.forEach({ team ->
             val board = jiraAgileClient.getBoard(team.jira.boardId)
 
-            val sprint = board.sprints.stream()
-                    .filter { sprint -> sprint.state == "active" }
-                    .findFirst()
-                    .orElseThrow { IllegalStateException("No active sprint found") }
-
-
-            val issues = sprint.issues
-                    .stream()
-                    .map { issue ->
-                        jiraClient.getIssue(issue.key, "", "changelog")
+            board.sprints.stream()
+                    .filter { sprint ->
+                        sprint.state == "active"
                     }
-                    .map { issue ->
-                        JiraIssue(
-                                issue.key,
-                                issue.summary,
-                                Optional.ofNullable(issue.assignee)
-                                        .map { user -> user.name }
-                                        .orElse("none"),
-                                JiraIssueType(
-                                        issue.issueType.name,
-                                        issue.issueType.iconUrl
+                    .findFirst()
+                    .map { sprint ->
+                        val issues = sprint.issues
+                                .stream()
+                                .map { issue ->
+                                    jiraClient.getIssue(issue.key, "", "changelog")
+                                }
+                                .map { issue ->
+                                    JiraIssue(
+                                            issue.key,
+                                            issue.summary,
+                                            Optional.ofNullable(issue.assignee)
+                                                    .map { user -> user.name }
+                                                    .orElse("none"),
+                                            JiraIssueType(
+                                                    issue.issueType.name,
+                                                    issue.issueType.iconUrl
+                                            ),
+                                            JiraIssueStatus(
+                                                    issue.status.name,
+                                                    issue.status.iconUrl
+                                            ),
+                                            JiraIssuePriority(
+                                                    issue.priority.name,
+                                                    issue.priority.iconUrl
+                                            ),
+                                            issue.changeLog.entries.stream()
+                                                    .flatMap { entry ->
+                                                        entry.items.stream().map { item ->
+                                                            JiraIssueChangeLogItemWithDate(entry.created, item)
+                                                        }
+                                                    }
+                                                    .filter { itemWithDate -> itemWithDate.item.field == "status" }
+                                                    .map { itemWithDate ->
+                                                        JiraIssueTransition(
+                                                                itemWithDate.created,
+                                                                itemWithDate.item.fromString,
+                                                                itemWithDate.item.toString
+                                                        )
+                                                    }
+                                                    .collect(Collectors.toList())
+                                    )
+                                }
+                                .collect(Collectors.toList())
+
+                        cache[team.name] = JiraBoard(
+                                JiraSprint(
+                                        sprint.name
                                 ),
-                                JiraIssueStatus(
-                                        issue.status.name,
-                                        issue.status.iconUrl
-                                ),
-                                JiraIssuePriority(
-                                        issue.priority.name,
-                                        issue.priority.iconUrl
-                                ),
-                                issue.changeLog.entries.stream()
-                                        .flatMap { entry ->
-                                            entry.items.stream().map { item ->
-                                                JiraIssueChangeLogItemWithDate(entry.created, item)
-                                            }
-                                        }
-                                        .filter { itemWithDate -> itemWithDate.item.field == "status" }
-                                        .map { itemWithDate ->
-                                            JiraIssueTransition(
-                                                    itemWithDate.created,
-                                                    itemWithDate.item.fromString,
-                                                    itemWithDate.item.toString
-                                            )
-                                        }
-                                        .collect(Collectors.toList())
+                                issues
                         )
                     }
-                    .collect(Collectors.toList())
+                    .orElseGet {
+                        cache[team.name] = JiraBoard(
+                                JiraSprint(
+                                        "No Active Sprint"
+                                ),
+                                ArrayList()
+                        )
+                    }
 
-            cache[team.name] = JiraBoard(
-                    JiraSprint(
-                            sprint.name
-                    ),
-                    issues
-            )
+
         })
     }
 }
