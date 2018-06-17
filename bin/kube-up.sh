87e0b4ea1b8b67972daff917ba5aa181f93463c7
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2018-06-17 23:45:24 +0100

    Shelly k8s config update

--- bin/kube-up.sh
+++ bin/kube-up.sh
@@ -4,6 +4,7 @@ kubectl create -f k8s/0-namespace.yml
 kubectl create -f k8s/1-config.yaml
 kubectl create -f k8s/2-backend.deployment.yaml
 kubectl create -f k8s/3-frontend.deployment.yaml
-kubectl create -f k8s/4-backend.service.yml
-kubectl create -f k8s/5-frontend.service.yml
-kubectl create -f k8s/6-ingress.yaml
+kubectl create -f k8s/4-shelly.deployment.yaml
+kubectl create -f k8s/5-backend.service.yml
+kubectl create -f k8s/6-frontend.service.yml
+kubectl create -f k8s/7-ingress.yaml
