ff366b3751dae06402a3ce64034a0502c4239896
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2018-06-17 23:48:53 +0100

    Add shelly to image build

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -2,8 +2,10 @@
 
 docker build -t fylmtm/team-dashboard-backend:${TRAVIS_TAG} backend/
 docker build -t fylmtm/team-dashboard-frontend:${TRAVIS_TAG} frontend/
+docker build -t fylmtm/team-dashboard-shelly:${TRAVIS_TAG} slack-bot-shelly/
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker push fylmtm/team-dashboard-backend:${TRAVIS_TAG}
 docker push fylmtm/team-dashboard-frontend:${TRAVIS_TAG}
+docker push fylmtm/team-dashboard-shelly:${TRAVIS_TAG}
