e963fbb3417c1913e8c8955ebdc1a43090fcb359
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2018-06-17 23:41:48 +0100

    Implement Shelly app status

--- slack-bot-shelly/src/commands/app-status.js
+++ slack-bot-shelly/src/commands/app-status.js
@@ -1,7 +1,27 @@
 const config = require('../config');
 const log = require('../util/log');
 const errorExit = require('../util/errorExit');
+const executeCommandSync = require('../util/executeCommandSync');
 
-module.exports = (app, environment) => {
-  log(`[${app} ${environment}] Not implemented.`);
+module.exports = (appName, askedEnv) => {
+  const app = config.apps.find(app => app.name === appName);
+  if (app == null) {
+    log(`App ${appName} not found.`);
+    errorExit();
+    return;
+  }
+
+  const foundEnv = app.environments.find(env => env.name === askedEnv);
+  if (foundEnv == null) {
+    log(`Environment ${askedEnv} is not configured for ${appName}.`);
+    errorExit();
+    return;
+  }
+
+  executeCommandSync(`helm status "${foundEnv.helmRelease}"`)
+    .then(stdout => log(stdout.toString()))
+    .catch(error => {
+      log(error.stdout.toString() + error.stderr.toString());
+      errorExit();
+    });
 };
